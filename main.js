6b5b4ebf2e41fbae9d948d185cb546267a7f4abb
sisj1869 <si1864@naver.comm>
2021-10-07 19:22:36 +0900

    V2

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const toogleBtn = document.querySelector('.navbar_toogleBtn');
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icons');
 
 toggleBtn.addEventListener('click', () => {
-    menu.classList.toogle('active');
-    icons.classList.toogle('active');
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
 });
+
+
